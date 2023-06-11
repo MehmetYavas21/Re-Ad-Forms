6eca89062de743580a2452c1c71d768271ad827d
Mehmet Yavas <107928102+MehmetYavas21@users.noreply.github.com>
2023-06-11 14:38:25 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,20 @@
+import React from 'react';
+import { useState } from 'react';
+
 function App() {
+  
+  const [quotes, setQuotes] = useState([]);
+
+
+
+  
   return (
     <div className="App">
       <h1>Our beautiful form will be here:</h1>
+      <form>
+        <textarea onChange={setQuote} style={{ placeholder:"add your quote" }}> {quote} </textarea>
+        <input onChange={setName} style={{ placeholder:"add your quote" }}> {name} </input>
+      </form>
       <h1>Quotes:</h1>
       <div className="quote">
         <p>
