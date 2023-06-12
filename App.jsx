7242179e82b136b6b194889414d702b97cdd35bd
Mehmet Yavas <107928102+MehmetYavas21@users.noreply.github.com>
2023-06-12 17:21:12 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -4,17 +4,35 @@ import { useState } from 'react';
 function App() {
   
   const [quotes, setQuotes] = useState([]);
+  const [name, setName] = useState([]);
 
-
-
+  const addQuote = (event) => {
+    event.preventDefault();
+    setQuotes({name:"name" , quote:"quotes"});
+  };
   
   return (
     <div className="App">
       <h1>Our beautiful form will be here:</h1>
-      <form>
-        <textarea onChange={setQuote} style={{ placeholder:"add your quote" }}> {quote} </textarea>
-        <input onChange={setName} style={{ placeholder:"add your quote" }}> {name} </input>
+    
+      <form onSubmit={addQuote}>
+        <textarea 
+          onChange={handleChange(event) => setQuotes(event.target.value)} 
+          style={{ placeholder:"add your quote" }}
+            > 
+              {quote} 
+        </textarea>
+        <input 
+          onChange={handleNameChange(event) => setName(event.target.value)} 
+          style={{ placeholder:"add your quote" }}
+            > 
+              {name} 
+        </input>
+        <button type="submit" >
+              "Add Quote"
+        </button>
       </form>
+
       <h1>Quotes:</h1>
       <div className="quote">
         <p>
