b121dd4ab31da0f54bdbba0c149b49d90e814f1b
Mehmet Yavas <107928102+MehmetYavas21@users.noreply.github.com>
2023-06-12 17:40:56 +0200

    solutions

--- App.jsx
+++ App.jsx
@@ -1,47 +1,58 @@
-import React from 'react';
 import { useState } from 'react';
 
 function App() {
   
-  const [quotes, setQuotes] = useState([]);
-  const [name, setName] = useState([]);
+  const [quotes, setQuotes] = useState([
+    {
+      quote:
+        "Ser dike azad dibe, azad dibe bedew dibe, bedew dibe tê hezkirin",
+      name: "Serok Tolhildan",
+    },
+  ]);
+  const [quote, setQuote] = useState("");
+  const [name, setName] = useState("");
 
   const addQuote = (event) => {
     event.preventDefault();
-    setQuotes({name:"name" , quote:"quotes"});
+    setQuotes((quotes) => [{name, quote} , ...quotes]);
+    setQuote("");
+    setName("");
   };
   
   return (
     <div className="App">
-      <h1>Our beautiful form will be here:</h1>
+      <h1>Share your favourite quote </h1>
     
       <form onSubmit={addQuote}>
         <textarea 
-          onChange={handleChange(event) => setQuotes(event.target.value)} 
+          onChange={(event) => setQuotes(event.target.value)} 
           style={{ placeholder:"add your quote" }}
-            > 
-              {quote} 
-        </textarea>
+          value={quote}
+         /> 
         <input 
-          onChange={handleNameChange(event) => setName(event.target.value)} 
-          style={{ placeholder:"add your quote" }}
-            > 
-              {name} 
-        </input>
+          onChange={(event) => setName(event.target.value)} 
+          style={{ placeholder:"Author" }}
+          value= {name} 
+          />
         <button type="submit" >
               "Add Quote"
         </button>
       </form>
 
-      <h1>Quotes:</h1>
-      <div className="quote">
-        <p>
-          My biggest fear is that people will attribute fake quotes to me and
-          millions of morons on the internet will believe it.
-        </p>
-        - <span>Albert Einstein</span>
+      <h1>Favourite Quotes:</h1>
+        {quotes.map((quote) => {
+          return (
+            <div className="quote" key={quote.quote}>
+                <p>
+                    {quote.quote}
+                </p>
+                - <span>
+                  {quote.name}
+                 </span>
+              </div>
+            );
+        })}
       </div>
-    </div>
   );
 }
 
